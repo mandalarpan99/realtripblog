28f13fd0782e9f8b74b85733098e32f436c4498f
mandalarpan99 <mandalarpan2014@gmail.com>
2025-11-04 23:30:39 +0530

    module change

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
-        base: "/realtripblog"
       },
     }),
   ],
+  base: "/realtripblog"
 })
